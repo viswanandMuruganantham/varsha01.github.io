ab5cb0e103a03d174240eb777061f92250170de2
viswanandMuruganantham <161728170+viswanandMuruganantham@users.noreply.github.com>
2024-02-29 20:09:24 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `WELCOME TO VARSHA'S CUTE CHANNEL, ${myName}`;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `WELCOME TO VARSHA'S CUTE CHANNEL, ${storedName}`;
+}
+myButton.onclick = () => {
+  setUserName();
+};
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `WELCOME TO VARSHA'S CUTE CHANNEL, ${myName}`;
+  }
+}
